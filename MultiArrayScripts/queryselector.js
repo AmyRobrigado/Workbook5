67525a147ec1ff93a929dc29f84b772cbd9680d2
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-11-09 15:06:09 -0500

    Added too many files

---
+++ MultiArrayScripts/queryselector.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// What does each return?
+let objSet1 = document.querySelectorAll("p");
+
+let objSet2 = document.querySelectorAll(".attn");
+
+let objSet3 = document.querySelectorAll("p.attn");
+
+let objSet4 = document.querySelectorAll("img[alt]");
+
+let objSet5 = document.querySelectorAll("div > p");
+
+let objSet6 = document.querySelectorAll("div, span"); 
+
+
+// Answers
+/* 
+1. Selects all <p> elements 
+
+2. Selects anything with an attn class 
+
+3. Selects the <p> element with an attention class 
+
+4. Selecting the alt information element
+
+5. Selects all <p> elements where the parent is the <div> element 
+
+6. Selects all <div> and <span> elements
+
+ */
